04f2fcaf2dac05521ca09c99423a8dd12049621a
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2024-05-31 09:40:29 +0100

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-"""Import datetime"""
 from datetime import datetime
-"""Import uuid"""
 import uuid
 
 """This is the base model for all classes"""
 class BaseModel:
     """Here goes the base attributes"""
-    def __init__(self):
-        """Initializing public attributes
 
-        args:
-           id: id of the instance
-           created_at: time when the instance is created
-           updated_at: time the instance is updated
-        """
+    def __init__(self):
+        """Initializing public attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def save(self):
-        """Updates the updated_at every time object
-           is changed with the current datetime
-        """
+        """Updates the updated_at every time object is changed"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
