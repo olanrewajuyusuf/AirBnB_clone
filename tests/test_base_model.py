af0893f9bc1a70cc4b0836f7591a3b983ad847ff
Wallewdev <wallewdev@gmail.com>
2024-06-12 23:32:03 +0100

    tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,74 +1,89 @@
-"""Test file for base_model"""
+#!/usr/bin/python3
+"""
+Module for BaseModel unittest
+"""
+import os
 import unittest
-from datetime import datetime
-from time import sleep
 from models.base_model import BaseModel
 
-class TestBaseModel(unittest.TestCase):
 
-    def test_id_is_unique(self):
-        """Test if id is unique for each instance"""
-        bm1 = BaseModel()
-        bm2 = BaseModel()
-        self.assertNotEqual(bm1.id, bm2.id)
 
-    def test_created_at_is_datetime(self):
-        """Test if created_at is a datetime object"""
-        bm = BaseModel()
-        self.assertIsInstance(bm.created_at, datetime)
+class TestBasemodel(unittest.TestCase):
+    """
+    Unittest for BaseModel
+    """
 
-    def test_updated_at_is_datetime(self):
-        """Test if updated_at is a datetime object"""
-        bm = BaseModel()
-        self.assertIsInstance(bm.updated_at, datetime)
+    def setUp(self):
+        """
+        Setup for temporary file path
+        """
+        try:
+            os.rename("file.json", "tmp.json")
+        except FileNotFoundError:
+            pass
 
-    def test_save_updates_updated_at(self):
-        """Test if save method updates updated_at"""
-        bm = BaseModel()
-        old_updated_at = bm.updated_at
-        sleep(0.1)
-        bm.save()
-        new_updated_at = bm.updated_at
-        self.assertNotEqual(old_updated_at, new_updated_at)
-        self.assertTrue(new_updated_at > old_updated_at)
+    def tearDown(self):
+        """
+        Tear down for temporary file path
+        """
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+        try:
+            os.rename("tmp.json", "file.json")
+        except FileNotFoundError:
+            pass
+    def test_init(self):
+        """
+        Test for init
+        """
+        my_model = BaseModel()
 
-    def test_str_method(self):
-        """Test the __str__ method for correct format"""
-        bm = BaseModel()
-        expected_str = f"[BaseModel] ({bm.id}) {bm.__dict__}"
-        self.assertEqual(str(bm), expected_str)
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
 
-    def test_to_dict_contains_correct_keys(self):
-        """Test if to_dict contains all necessary keys and correct types"""
-        bm = BaseModel()
-        bm_dict = bm.to_dict()
-        self.assertEqual(bm_dict['__class__'], 'BaseModel')
-        self.assertEqual(bm_dict['id'], bm.id)
-        self.assertEqual(bm_dict['created_at'], bm.created_at.isoformat())
-        self.assertEqual(bm_dict['updated_at'], bm.updated_at.isoformat())
-        self.assertIsInstance(bm_dict['created_at'], str)
-        self.assertIsInstance(bm_dict['updated_at'], str)
+    def test_save(self):
+        """
+        Test for save method
+        """
+        my_model = BaseModel()
 
-    def test_to_dict_output(self):
-        """Test the output of to_dict method"""
-        bm = BaseModel()
-        bm_dict = bm.to_dict()
-        expected_keys = ['id', 'created_at', 'updated_at', '__class__']
-        self.assertTrue(all(key in bm_dict for key in expected_keys))
-        self.assertEqual(bm_dict['__class__'], 'BaseModel')
-        self.assertEqual(bm_dict['id'], bm.id)
-        self.assertEqual(bm_dict['created_at'], bm.created_at.isoformat())
-        self.assertEqual(bm_dict['updated_at'], bm.updated_at.isoformat())
+        initial_updated_at = my_model.updated_at
 
-    def test_recreate_instance_from_dict(self):
-        """Test if an instance can be recreated from a dictionary"""
-        bm1 = BaseModel()
-        bm1_dict = bm1.to_dict()
-        bm2 = BaseModel(**bm1_dict)
-        self.assertEqual(bm1.id, bm2.id)
-        self.assertEqual(bm1.created_at, bm2.created_at)
-        self.assertEqual(bm1.updated_at, bm2.updated_at)
-        self.assertNotIn('__class__', bm2.__dict__)
+        current_updated_at = my_model.save()
 
-if __name__ == '__main__':
+        self.assertNotEqual(initial_updated_at, current_updated_at)
+
+    def test_to_dict(self):
+        """
+        Test for to_dict method
+        """
+        my_model = BaseModel()
+
+        my_model_dict = my_model.to_dict()
+
+        self.assertIsInstance(my_model_dict, dict)
+
+        self.assertEqual(my_model_dict["__class__"], 'BaseModel')
+        self.assertEqual(my_model_dict['id'], my_model.id)
+        self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict["updated_at"], my_model.created_at.isoformat())
+
+
+    def test_str(self):
+        """
+        Test for string representation
+        """
+        my_model = BaseModel()
+
+        self.assertTrue(str(my_model).startswith('[BaseModel]'))
+
+        self.assertIn(my_model.id, str(my_model))
+
+        self.assertIn(str(my_model.__dict__), str(my_model))
+
+
+if __name__ == "__main__":
     unittest.main()
